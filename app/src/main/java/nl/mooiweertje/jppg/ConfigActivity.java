b3723a205d5eb23abdf701e4df2606aa13e14cc8
mooiweertje <github@mooiweertje.nl>
2022-11-25 20:01:58 +0100

    initial hack, config activity

---
+++ app/src/main/java/nl/mooiweertje/jppg/ConfigActivity.java
@@ -0,0 +1,42 @@
+package nl.mooiweertje.jppg;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+public class ConfigActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("SettingActive!");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent keyEvent) {
+        System.out.println("Key: " + keyEvent.toString());
+        return false;
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            System.out.println("onCreatePreferences!");
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
